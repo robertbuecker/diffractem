eae938df5490b211c13de840bb7785bced82d047
Robert Buecker <bueckerr@cfelm-pcx30388.desy.de>
2020-04-03 17:28:03 +0200

    proc2d.stack_nested: recursively stack/concat

--- diffractem/proc2d.py
+++ diffractem/proc2d.py
@@ -13,6 +13,16 @@ from typing import Optional, Tuple, Union, List, Callable
 from warnings import warn
 from onda.algorithms.crystallography_algorithms import Peakfinder8PeakDetection
 
+def stack_nested(data_list, concat=False):    
+    if isinstance(data_list[0], tuple):
+        return tuple(stack_nested(arr, concat) for arr in zip(*data_list))
+    elif isinstance(data_list[0], list):
+        return list(stack_nested(arr, concat) for arr in zip(*data_list))
+    elif isinstance(data_list[0], dict):
+        return {k: stack_nested(list(o[k] for o in data_list), concat) for k in data_list[0].keys()}
+    else:
+        return np.concatenate(data_list) if concat else np.stack(data_list)
+
 def loop_over_stack(fun):
     """
     Decorator to (sequentially) loop a 2D processing function over a stack. 
@@ -39,11 +49,11 @@ def loop_over_stack(fun):
         if imgs.ndim == 2:
             return fun(imgs, *args, **kwargs)
 
-        elif imgs.shape[0] == 1:
-            # some gymnastics if arrays are on weird dimensions (often after map_blocks)
-            args = [a.squeeze() if isinstance(a, np.ndarray) else a for a in args]
-            kwargs = {k: a.squeeze() if isinstance(a, np.ndarray) else a for k, a in kwargs.items()}
-            return np.expand_dims(fun(imgs.squeeze(), *args, **kwargs), axis=0)
+        # elif imgs.shape[0] == 1:
+        #     # some gymnastics if arrays are on weird dimensions (often after map_blocks)
+        #     args = [a.squeeze() if isinstance(a, np.ndarray) else a for a in args]
+        #     kwargs = {k: a.squeeze() if isinstance(a, np.ndarray) else a for k, a in kwargs.items()}
+        #     return np.expand_dims(fun(imgs.squeeze(), *args, **kwargs), axis=0)
 
         #print('Applying {} to {} images of shape {}'.format(fun, imgs.shape[0], imgs.shape[1:]))
 
@@ -89,7 +99,9 @@ def loop_over_stack(fun):
             return np.ndarray(imgs.shape, dtype=imgs.dtype)
 
         try:
-            print(type(out), type(out[0]))
+            # print(type(out), type(out[0]))
+            return stack_nested(out)
+
             if isinstance(out[0], np.ndarray):
                 return np.stack(out)
             elif isinstance(out[0], tuple):
