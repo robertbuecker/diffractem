d3bba5b9567efa41865ea980205efd225f3935ca
Robert Bücker <robert.buecker@mpsd.mpg.de>
2020-04-24 21:05:02 +0200

    better doc

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,8 @@ author = 'Robert Buecker'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
