91a45237722c6d721489d41e543bb33dfd3dc85d
Robert Bücker <robert.buecker@mpsd.mpg.de>
2020-04-24 20:16:46 +0200

    more doc

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autodoc_mock_imports = ['PyQt5']
 
 # -- Options for HTML output -------------------------------------------------
 
