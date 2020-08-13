4584330091ea62d8c1e26b9e70aabeae2e8174f9
Robert Buecker <bueckerr@cfelm-pcx30388.desy.de>
2020-08-13 09:50:25 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='diffractem',
-    version='0.3.1',
+    version='0.3.2',
     packages=['diffractem'],
     url='https://github.com/robertbuecker/diffractem',
     license='',
