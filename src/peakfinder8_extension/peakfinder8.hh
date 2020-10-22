b4e9551faa036ed19e9e45b12cb349461e1f3867
Robert Bücker <robert.buecker@mpsd.mpg.de>
2020-10-22 13:20:45 +0200

    updated license remarks in pf8 files

--- src/peakfinder8_extension/peakfinder8.hh
+++ src/peakfinder8_extension/peakfinder8.hh
@@ -1,20 +1,24 @@
-//    This file is part of OnDA.
+//    This file is originally part of OnDA, available at
+//    <https://www.ondamonitor.com>, which is released under the terms
+//    of the GNU General Public License. It has been adapted for
+//    use in diffractem.
 //
-//    OnDA is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
+//	  In agreement with the authors, you can redistribute it and/or modify
+//    this file under the terms of the GNU Lesser General Public License 
+//    as published by the Free Software Foundation, either version 3 
+//    of the License, or (at your option) any later version.
 //
-//    OnDA is distributed in the hope that it will be useful,
+//    It is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
+//    GNU Lesser General Public License for more details.
 //
-//    You should have received a copy of the GNU General Public License
-//    along with OnDA.  If not, see <http://www.gnu.org/licenses/>.
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with diffractem.  If not, see <http://www.gnu.org/licenses/>.
 //
 //    Copyright 2014-2019 Deutsches Elektronen-Synchrotron DESY,
 //    a research centre of the Helmholtz Association.
+
 #ifndef PEAKFINDER8_H
 #define PEAKFINDER8_H
 
